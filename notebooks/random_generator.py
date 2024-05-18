1e058a71e32d35591f5159691338535aa373432f
Bartoszcz28 <bartoszczarnecki26@gmail.com>
2024-05-18 16:53:34 +0200

    random generator update

--- notebooks/random_generator.py
+++ notebooks/random_generator.py
@@ -14,8 +14,7 @@ atm = pd.read_csv("notebooks/data/atm.csv")
 country = pd.read_csv("notebooks/data/country.csv")
 person = pd.read_csv("notebooks/data/person.csv")
 
-result = pd.merge(person, country, how="inner", on=["country_id", "country_id"])
-result = pd.merge(result, atm, how="inner", on=["country_id", "country_id"])
+result = pd.merge(atm, country, how="inner", on=["country_id", "country_id"])
 
 if __name__ == "__main__":
     SERVER = "broker:9092"
@@ -33,25 +32,28 @@ if __name__ == "__main__":
             message_id += 1
             t = datetime.now() + timedelta(seconds=random.randint(-15, 0))
 
-            # Losowy wybór klienta z DataFrame result
-            client_id = random.choice(result["customer_id"].unique())
-
-            # Wybór wierszy z DataFrame result odpowiadających wybranemu klientowi
-            customer_info = result[result["customer_id"] == client_id].iloc[0]
-
+            # Random customer selection from DataFrame person
+            client_id = random.choice(person["customer_id"].unique())
+            
+            # Selecting rows from the DataFrame result corresponding to the selected client
+            customer_info = result.sample(n=1).iloc[0]
+            
+            # Selecting a row from the DataFrame person corresponding to the selected client
+            person_info = person[person["customer_id"] == client_id].iloc[0]
+            
             message = {
                 "time": str(t),
-                "message_id": int(message_id),  # Konwersja na int
-                "client_id": int(client_id),  # Konwersja na int
-                "amount": int(random.randint(-1000, 1000)),  # Konwersja na int
-                "first_name": str(customer_info["first_name"]),
-                "last_name": str(customer_info["last_name"]),
-                "email": str(customer_info["email"]),
-                "gender": str(customer_info["gender"]),
-                "country_id": int(customer_info["country_id"]),  # Konwersja na int
+                "message_id": int(message_id), # Conversion to int
+                "client_id": int(client_id),  # Conversion to int
+                "amount": int(random.randint(-1000, 1000)),  # Conversion to int
+                "first_name": str(person_info["first_name"]),
+                "last_name": str(person_info["last_name"]),
+                "email": str(person_info["email"]),
+                "gender": str(person_info["gender"]),
+                "country_id": int(customer_info["country_id"]),  # Conversion to int
                 "country": str(customer_info["country"]),
                 "capital": str(customer_info["capital"]),
-                "atm_id": int(customer_info["atm_id"]),  # Konwersja na int
+                "atm_id": int(customer_info["atm_id"]),  # Conversion to int
                 "atm_number": str(customer_info["atm_number"]),
                 "aml": int(0),
             }
